e1cbd26a3d437aa387f248bb08b36ad7c5337dc1
kigari94 <36a8d9lf21x>
2022-01-20 15:38:58 +0100

    Fix css loading issue / rearrange structure to static > css

--- src/main/java/com/haw/one4all/configuration/WebConfiguration.java
+++ src/main/java/com/haw/one4all/configuration/WebConfiguration.java
@@ -10,35 +10,9 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-@EnableWebMvc
 @Configuration
 @ComponentScan({"com.haw.one4all"})
 public class WebConfiguration implements WebMvcConfigurer {
 
-//    @Bean
-//    @Description("Thymeleaf Template Resolver")
-//    public ServletContextTemplateResolver templateResolver() {
-//        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-//        templateResolver.setPrefix("");
-//        templateResolver.setSuffix(".html");
-//        templateResolver.setTemplateMode("HTML5");
-//
-//        return templateResolver;
-//    }
-//
-//    @Bean
-//    @Description("Thymeleaf Template Engine")
-//    public SpringTemplateEngine templateEngine() {
-//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-//        templateEngine.setTemplateResolver(templateResolver());
-//        return templateEngine;
-//    }
-//    @Bean
-//    @Description("Thymeleaf View Resolver")
-//    public ThymeleafViewResolver viewResolver() {
-//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-//        viewResolver.setTemplateEngine(templateEngine());
-//        viewResolver.setOrder(1);
-//        return viewResolver;
-//    }
+
 }
--- src/main/java/com/haw/one4all/controller/HomePageController.java
+++ src/main/java/com/haw/one4all/controller/HomePageController.java
@@ -1,16 +1,13 @@
 package com.haw.one4all.controller;
 
-import com.haw.one4all.Model.HomePageModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomePageController {
 
     @GetMapping("/")
-    public String home(@ModelAttribute("model") HomePageModel model) {
-//        model.setTitle("Test");
-        return "view/home";
+    public String showHome() {
+        return "views/home";
     }
 }
