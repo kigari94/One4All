f1ede0ab25d004ada2c0f429e8e3a978bde6bb90
Na143 <nargis.hossainy@haw-hamburg.de>
2022-01-28 11:50:32 +0100

    Impressum Muster

--- src/main/java/com/haw/one4all/controller/ImpressumController.java
+++ src/main/java/com/haw/one4all/controller/ImpressumController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ImpressumController {
 
     @GetMapping("/impressum")
-    public String start() {
+    public String show() {
         return "views/impressum";
     }
 }
