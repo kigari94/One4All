f16e9143102dd4ded3010f51275a69123850857c
kigari94 <36a8d9lf21x>
2022-02-04 21:46:23 +0100

    prevent logged in user from reaching registration/login, adding cypress cases

--- src/main/java/com/haw/one4all/controller/LoginController.java
+++ src/main/java/com/haw/one4all/controller/LoginController.java
@@ -1,13 +1,23 @@
 package com.haw.one4all.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String showHome() {
-        return "views/login";
+    public String showLogin() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "views/login";
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/haw/one4all/controller/RegistrationController.java
+++ src/main/java/com/haw/one4all/controller/RegistrationController.java
@@ -3,6 +3,9 @@ package com.haw.one4all.controller;
 import com.haw.one4all.Model.User;
 import com.haw.one4all.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +25,13 @@ public class RegistrationController {
 
     @GetMapping("/register")
     public String showRegistration() {
-        return "views/registration";
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "views/registration";
+        }
+
+        return "redirect:/";
     }
 
     @PostMapping("/process_register")
