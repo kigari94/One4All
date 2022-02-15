eb713a8cd79d04acff1661933f51f4e149dac9fc
Karim-zcm <karimmalik50@yahoo.de>
2022-02-15 15:05:27 +0100

    F460 Filter für gängige Schimpfwörter, Wörter die zur Hassrede verwendet werden können und URL's

--- src/main/java/com/haw/one4all/controller/ProjectPageController.java
+++ src/main/java/com/haw/one4all/controller/ProjectPageController.java
@@ -1,12 +1,14 @@
 package com.haw.one4all.controller;
 import com.haw.one4all.Model.Project;
 import com.haw.one4all.Model.User;
+import com.haw.one4all.service.ProjectInputFilter;
 import com.haw.one4all.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,16 +31,35 @@ public class ProjectPageController {
 
     @PostMapping("/createProject")
     public String processRegister(@Valid Project project, BindingResult result, User user) {
-        // Validating field values
-        if (result.hasErrors()) {
-            return "views/projectPage";
-        }
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         user.setUsername(authentication.getName());
         project.setUser(user);
 
+        // check title inappropriate words
+        if (!ProjectInputFilter.isValid(project.getTitle())) {
+            result.addError(new FieldError("project", "title",
+                    "Der Titel enthält unangebrachte oder anstößige Inhalte"));
+        }
+
+        // check description for inappropriate words
+        if (!ProjectInputFilter.isValid(project.getDescription())) {
+            result.addError(new FieldError("project", "description",
+                    "Die Beschreibung enthält unangebrachte oder anstößige Inhalte"));
+        }
+
+        // checking crew for inappropriate words
+        if (!ProjectInputFilter.isValid(project.getCrew())) {
+            result.addError(new FieldError("project", "crew",
+                    "Die Gewerke enthalten unangebrachte oder anstößige Inhalte"));
+        }
+
+        // Validating field values
+        if (result.hasErrors()) {
+            return "views/projectPage";
+        }
+
         projectService.saveProject(project);
         return "redirect:/";
-
     }
 }
---
+++ src/main/java/com/haw/one4all/service/ProjectInputFilter.java
@@ -0,0 +1,36 @@
+package com.haw.one4all.service;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ProjectInputFilter {
+
+    // list of statistically common inappropriate/forbidden words
+    static String[] filterList = {
+        "dumm", "terror", "isis", "nazi", "arsch", "bastard", "fuck", "shit", "idiot", "scheiße", "arschloch", "bitch", "krüppel", "nutte"
+    };
+
+    // regex to filter URL's
+    private static final String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+    private static final Pattern pattern = Pattern.compile(regex);
+
+    // check for inappropriate words in userinput
+    public static boolean isValid(final String input){
+        String[] inputList = input.split(" ");
+        for (String word : inputList){
+            // filter URL's
+            Matcher matcher = pattern.matcher(word);
+            if (matcher.matches()) { return false; }
+
+            // match the words in the user input to the list of bad words
+            for (String badWord : filterList){
+                if (word.toLowerCase().equals(badWord.toLowerCase())){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
